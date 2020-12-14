5b468f96a72c564f49310c8fd0d81f6ef77be224
James Belta <james.belta@gmail.com>
2020-12-14 10:02:38 -0700

    Food service connection, end path and parse

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,14 +1,18 @@
 class FoodService
 
   def self.food(ingredient)
-    response = conn.get('/v1/foods/search') do |req|
+    binding.pry
+    response = conn.get('v1/foods/search') do |req|
       req.params[:query] = ingredient
+    end
+    binding.pry
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   private
 
   def self.conn
-    Faraday.new('https://api.nal.usda.gov/fdc') do |f|
+    Faraday.new('https://api.nal.usda.gov/fdc/') do |f|
       f.headers['x-api-key'] = ENV['USDA_API_KEY']
     end
   end
